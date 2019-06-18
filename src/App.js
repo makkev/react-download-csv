cb1fd77b3f09e7606271fcec8df6344ce191afb5
K Mak <kumarajiva08@gmail.com>
2019-06-19 07:07:09 +1000

    download csv

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var stockData = [
+    {
+        Symbol: "AAPL",
+        Company: "Apple Inc.",
+        Price: "132.54"
+    },
+    {
+        Symbol: "INTC",
+        Company: "Intel Corporation",
+        Price: "33.45"
+    },
+    {
+        Symbol: "GOOG",
+        Company: "Google Inc",
+        Price: "554.52"
+    },
+  ];
+
+  const convertArrayOfObjectsToCSV = args =>  {
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+        return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ',';
+    lineDelimiter = args.lineDelimiter || '\n';
+
+    keys = Object.keys(data[0]);
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(function(item) {
+        ctr = 0;
+        keys.forEach(function(key) {
+            if (ctr > 0) result += columnDelimiter;
+
+            result += item[key];
+            ctr++;
+        });
+        result += lineDelimiter;
+    });
+
+    return result;
+  }
+
+  var data, filename;
+
+  var csv = convertArrayOfObjectsToCSV({
+      data: stockData
+  });
+
+  filename = 'export.csv';
+
+  if (!csv.match(/^data:text\/csv/i)) {
+      csv = 'data:text/csv;charset=utf-8,' + csv;
+  }
+  data = encodeURI(csv);
+
+  console.log('data: ', data);
+  console.log('filename: ', filename);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href={data} download={filename} >Download CSV</a>
     </div>
   );
 }
